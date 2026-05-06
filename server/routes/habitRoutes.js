2f3909cf61aea4d667cf20408bfa2b212a25b0e1
Adrián Marín Hurtado <amarinhurtado@gmail.com>
2026-05-06 13:41:01 +0200

    Ajuste en código.

--- server/routes/habitRoutes.js
+++ server/routes/habitRoutes.js
@@ -4,18 +4,13 @@ import {
   createHabit,
   updateHabit,
   deleteHabit,
-  getHabitRecords,
-  createHabitRecord,
 } from "../controllers/habitController.js"
 
 const router = express.Router()
 
-// hábitos
 router.get("/habits", getHabits)
 router.post("/habits", createHabit)
 router.patch("/habits/:id", updateHabit)
 router.delete("/habits/:id", deleteHabit)
-router.get("/habits/:id/records", getHabitRecords)
-router.post("/habits/:id/records", createHabitRecord)
 
 export default router
